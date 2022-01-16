ecbab61292864826697e986b6809de443bf444dc
Muhammad Ariananda <m.ariananda079@gmail.com>
2022-01-16 22:23:07 +0800

    initial commit

---
+++ app/src/main/java/id/muhariananda/notemarks/MainActivity.kt
@@ -0,0 +1,11 @@
+package id.muhariananda.notemarks
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
